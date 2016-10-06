aabe9680d712d5c46a5990056e32d6cdcefd27eb
Kornosk130 <kornosk130@gmail.com>
2016-10-06 19:36:58 +0700

    Edit myJSON code

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 $('.sidebar-controls').children()[1].click();
+
+// Close the alert modal
 setInterval(function(){
 	try{
 		$('.modal-footer.ng-scope').children()[0].click();
@@ -7,10 +9,12 @@ setInterval(function(){
 	}
 },1000);
 
+// Refresh for updated location
 setInterval(function(){
 	if($('.icon.icon-refresh')[1].parentNode.parentNode.parentNode.children[0].children[1].innerText == "Kornraphop Kawintiranon ADMIN"){
 		if($('.family-members').children()[2].children[1].children[2].className == "ng-hide") {
 			var text = $('.family-members').children()[2].children[1].children[1].innerText;
+			console.log(text);
 			var obj = {lat: parseFloat(text.split("\n")[0].substring(text.split("\n")[0].indexOf(" ")+1)), lng: parseFloat(text.split("\n")[1].substring(text.split("\n")[1].indexOf(" ")+1)), time: Date.now()};
 			if(obj.lat > 13.733766 && obj.lat < 13.743240 && obj.lng > 100.527844 && obj.lng < 100.536406) {
 				updateJSON(JSON.stringify(obj))
@@ -27,7 +31,7 @@ setInterval(function(){
 // Update location and upload to myJSON server
 function updateJSON(x){
 	$.ajax({
-    		url:"https://api.myjson.com/bins/2f5ag",
+    		url:"https://api.myjson.com/bins/2vup0",
     		type:"PUT",
     		data: x,
     		contentType:"application/json; charset=utf-8",
